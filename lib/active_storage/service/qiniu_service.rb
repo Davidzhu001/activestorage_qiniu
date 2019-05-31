154fe54ef73220f391fdadeaf4c26e2d1cbaf7f4
Weicheng Zhu <weicheng.zhu@icloud.com>
2019-05-31 16:09:57 +0800

    Update qiniu_service.rb

--- lib/active_storage/service/qiniu_service.rb
+++ lib/active_storage/service/qiniu_service.rb
@@ -113,7 +113,7 @@ module ActiveStorage
                 attname = URI.escape "#{options[:filename] || key}"
                 "attname=#{attname}"
               end
-                      fop ||= 'imageView2/1/w/800/h/800/auto-orient'
+                      fop ||= 'imageView2/1/w/1500/h/1500/auto-orient'
 
       
         
