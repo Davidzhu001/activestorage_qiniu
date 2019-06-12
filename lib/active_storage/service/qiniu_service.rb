d61515bebe45e44ff918d0b4ab570fa049e23fd0
davidzhu001 <weicheng.zhu@icloud.com>
2019-06-12 18:57:07 +0800

    update to new

--- lib/active_storage/service/qiniu_service.rb
+++ lib/active_storage/service/qiniu_service.rb
@@ -45,11 +45,11 @@ module ActiveStorage
         begin
           upload_method = io.respond_to?(:read) ? :upload_buffer_with_token : :upload_with_token_2
           code, result, response_headers = Qiniu::Storage.send(upload_method,
-            generate_uptoken(key),
-            io,
-            key,
-            nil,
-            bucket: bucket
+                                                               generate_uptoken(key),
+                                                               io,
+                                                               key,
+                                                               nil,
+                                                               bucket: bucket
           )
 
           result
@@ -68,7 +68,7 @@ module ActiveStorage
 
     def delete_prefixed(prefix)
       instrument :delete_prefixed, prefix: prefix do
-        items_for(prefix).each { |item| delete item['key'] }
+        items_for(prefix).each {|item| delete item['key']}
       end
     end
 
@@ -107,16 +107,13 @@ module ActiveStorage
 
     def url(key, **options)
       instrument :url, key: key do |payload|
-              fop = if options[:fop].present?        # 内容预处理
+        fop = if options[:fop].present? # 内容预处理
                 options[:fop]
               elsif options[:disposition].to_s == 'attachment' # 下载附件
                 attname = URI.escape "#{options[:filename] || key}"
                 "attname=#{attname}"
               end
-                      fop ||= 'imageView2/1/w/200/h/200/auto-orient'
 
-      
-        
         url = if bucket_private
                 expires_in = options[:expires_in] || url_expires_in
                 Qiniu::Auth.authorize_download_url_2(domain, key, schema: protocol, fop: fop, expires_in: expires_in)
@@ -139,23 +136,23 @@ module ActiveStorage
     end
 
     def headers_for_direct_upload(key, content_type:, checksum:, **)
-      { "Content-Type" => content_type, "Content-MD5" => checksum, "x-token" => generate_uptoken(key) }
+      {"Content-Type" => content_type, "Content-MD5" => checksum, "x-token" => generate_uptoken(key)}
     end
 
     private
 
-    def items_for(prefix='')
+    def items_for(prefix = '')
       list_policy = Qiniu::Storage::ListPolicy.new(
-        bucket,   # 存储空间
-        1000,     # 列举的条目数
-        prefix,   # 指定前缀
-        ''        # 指定目录分隔符
+          bucket, # 存储空间
+          1000, # 列举的条目数
+          prefix, # 指定前缀
+          '' # 指定目录分隔符
       )
       code, result, response_headers, s, d = Qiniu::Storage.list(list_policy)
       result['items']
     end
 
-    def generate_uptoken(key=nil, expires_in=nil)
+    def generate_uptoken(key = nil, expires_in = nil)
       expires_in ||= 3600
       put_policy = Qiniu::Auth::PutPolicy.new(bucket, key, expires_in)
       upload_options.slice(*Qiniu::Auth::PutPolicy::PARAMS.keys).each do |k, v|
